0d24be932af8ba9527e3893c370bc5c5b09a8eb9
Nelly <fehlenlili64100@gmail.com>
2020-11-25 16:37:33 +0100

    ise en place frontend vueJs

---
+++ frontend/src/components/Accueil.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-app id="accueil" class="text-center">
+        <top-header/>
+        <h1 >Välkommen</h1>
+        <router-view></router-view>
+         
+    </v-app>
+</template>
+<script>
+import TopHeader from "./Accueil/TopHeader"
+
+export default {
+    name: "Accueil",
+    data(){
+        return{
+            
+        }
+    },
+    components: {
+        "top-header": TopHeader,
+    },
+}
+</script>
+<style lang="scss">
+    #accueil{
+        background: no-repeat center/80% url("../assets/icon-left-font-monochrome-black.svg") ;
+    }
+    .header{
+        background-color: rgb(94, 85, 83);
+        color: white;
+    }
+        
+    
+</style>
---
+++ frontend/src/components/Accueil/FormPost.vue
@@ -0,0 +1,78 @@
+<template>
+    <v-app id="formPost">
+        <top-header/>
+        <h1 class="ml-12">Le mur</h1>
+        <v-card class="ma-3 ml-12">
+            <v-card-title class="mb-3">
+                <h2>Nouveau post </h2>
+            </v-card-title>
+            
+            <v-card-text>
+                <v-form ref="form" class="ma-3" v-model="valid" >
+                    <v-text-field v-model="dataPost.title" :rules="titleRules" :counter="50" label="Title" autofocus required></v-text-field>
+                    <v-textarea v-model="dataPost.content" :rules="contentRules" label="Content" required></v-textarea>
+                </v-form>
+            </v-card-text>
+
+            <v-card-actions>
+                <v-btn  :disabled="!valid" class="success" @click="sendPost">Poster</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-app>
+</template>
+<script>
+import axios from "axios"
+import TopHeader from "./TopHeader"
+
+
+export default {
+    name: "FormPost",
+    data(){
+        return{
+            valid: true,
+            titleRules: [
+                v => !!v || 'Title is required',
+                v => (v && v.length <= 50) || 'Title must be less than 50 characters',
+
+            ],
+            contentRules: [
+                v => !!v || 'Content is required',
+            ],
+            dataPost:{
+                title: "",
+                content:"",
+                userId: localStorage.userId
+            },
+            dataPostS: "",
+            msg: false,
+            message: "",
+        }
+    },
+    methods: {
+        sendPost(){
+            this.dataPostS = JSON.stringify(this.dataPost);
+            axios.post("http://localhost:3000/api/posts/", this.dataPostS, {headers: {'Content-Type': 'application/json', Authorization: 'Bearer ' + localStorage.token}})
+                .then(response => {
+                    let rep = JSON.parse(response.data);
+                    this.message = rep.message;
+                    this.msg = true;
+                    this.form = false;
+                    this.$router.push('/Accueil/Mur')
+                    
+                })
+                .catch(error => {
+                    console.log(error); 
+                    this.message = error;
+                    this.msg = true
+                });
+        },
+    },
+    components: {
+        "top-header": TopHeader, 
+        
+    },
+}
+</script>
+<style lang="scss">
+
+</style>
